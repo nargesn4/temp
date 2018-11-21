34ce24b6bc3249e509e1b4b0af2f166071c82e83
nargesn4 <nargesjafarie@gmail.com>
2018-11-21 15:15:38 +0100

    tweede commit

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,27 +1,27 @@
 import { CanActivate, Router } from '@angular/router';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireAuth } from "@angular/fire/auth";
 import { Injectable } from "@angular/core";
-import { Observable, from as observableFrom} from "rxjs";
-import { auth } from 'firebase/app';
-import * as firebase from 'firebase/app';
+import {Observable,  from} from "rxjs";
 // import 'rxjs/add/operator/do';
 // import 'rxjs/add/operator/map';
 // import 'rxjs/add/operator/take';
+// import 'rxjs/add/observable/from';
+console.log("not yet implemented");
 
-@Injectable()
-export class AuthGuard {
-  // implements CanActivate {
-  //
-  // constructor(private auth: AngularFireAuth, private router: Router) {}
-  //
-  // canActivate(): Observable<boolean> {
-  //   return observableFrom(this.auth)
-  //     .take(1)
-  //     .map(state => !!state)
-  //     .do(authenticated => {
-  //       if
-  //       (!authenticated) this.router.navigate([ '/login' ]);
-  //     })
-  // }
-
-}
+// @Injectable()
+export class AuthGuard {}
+// implements CanActivate {
+//
+//   constructor(private auth: AngularFireAuth, private router: Router) {}
+//
+//   canActivate(): Observable<boolean> {
+//     return from(this.auth)
+//       .take(1)
+//       .map(state => !!state)
+//       .do(authenticated => {
+//         if
+//         (!authenticated) this.router.navigate([ '/login' ]);
+//       })
+//   }
+//
+// }
